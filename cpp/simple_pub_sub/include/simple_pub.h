c576789c1d9f98f1124f8693ff7f5292813fc939
mrzu <thomas_busser@hotmail.com>
2024-10-22 16:07:49 -0700

    Copied most recent

--- cpp/simple_pub_sub/include/simple_pub.h
+++ cpp/simple_pub_sub/include/simple_pub.h
@@ -1,12 +1,16 @@
+/*
+
+  This is the starting point for your Unit. Edit this directly and implement the
+  missing methods!
+
+*/
 #include <unit/simple_pub/unit_base.h>
 
 class simple_pub : public unit::simple_pub::Base {
 public:
-  simple_pub() {
+  simple_pub(const unit::simple_pub::Args& args, const std::optional<std::string_view>& name_override = {})
+      : unit::simple_pub::Base(args, name_override) {}
 
-  }
-
-
-    virtual unit::simple_pub::PublishAt1Hz::Output PublishAt1Hz(const unit::simple_pub::PublishAt1Hz::Input& input) override;
-
+  virtual unit::simple_pub::PublishAt1Hz::Output
+  PublishAt1Hz(const unit::simple_pub::PublishAt1Hz::Input &input) override;
 };
--- cpp/simple_pub_sub/include/simple_sub.h
+++ cpp/simple_pub_sub/include/simple_sub.h
@@ -1,12 +1,16 @@
+/*
+
+  This is the starting point for your Unit. Edit this directly and implement the
+  missing methods!
+
+*/
 #include <unit/simple_sub/unit_base.h>
 
 class simple_sub : public unit::simple_sub::Base {
 public:
-  simple_sub() {
+  simple_sub(const unit::simple_sub::Args& args, const std::optional<std::string_view>& name_override = {})
+      : unit::simple_sub::Base(args, name_override) {}
 
-  }
-
-
-    virtual unit::simple_sub::OnChatter::Output OnChatter(const unit::simple_sub::OnChatter::Input& input) override;
-
+  virtual unit::simple_sub::OnChatter::Output
+  OnChatter(const unit::simple_sub::OnChatter::Input &input) override;
 };
